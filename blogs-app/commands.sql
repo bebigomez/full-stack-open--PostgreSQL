e6c18cd72e8e601f17eae6e32e6b7068395ff9b5
Victor Gomez <vhgomez.py@gmail.com>
2024-06-26 09:47:28 -0400

    13.3 done

---
+++ blogs-app/commands.sql
@@ -0,0 +1,13 @@
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    author TEXT,
+    url TEXT NOT NULL,
+    title TEXT NOT NULL,
+    likes INT DEFAULT 0
+);
+
+INSERT INTO blogs (title, author, url) VALUES ('First ever blog', 'bebi', 'https://bebidev.netlify.app');
+
+INSERT INTO blogs (title, author, url) VALUES ('Second blog post', 'John Doe', 'https://example.com/second-blog');
+
+SELECT * FROM blogs;
