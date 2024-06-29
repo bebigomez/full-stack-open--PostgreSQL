f18adeac163738030af60bd7eb20548f8d49e173
Victor Gomez <vhgomez.py@gmail.com>
2024-06-29 12:25:59 -0400

    13.7 done

--- blogs-app/controllers/blogs.js
+++ blogs-app/controllers/blogs.js
@@ -17,24 +17,44 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-router.put('/:id', async (req, res) => {
+router.post('/', async (req, res, next) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    //return res.status(400).json({ error })
+    next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
   const blog = await Blog.findByPk(req.params.id)
   if (blog) {
-    blog.likes = req.body.likes
-    await blog.save()
-    res.json(blog)
+    try {
+      blog.likes = req.body.likes
+      await blog.save()
+      res.json(blog)
+    } catch (error) {
+      //return res.status(400).json({ error })
+      next(error)
+    }
   } else {
     res.status(404).end()
   }
 })
 
-router.post('/', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    return res.json(blog)
-  } catch (error) {
-    return res.status(400).json({ error })
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'SequelizeValidationError') {
+    return res.status(422).json({ error: 'Validation error' })
+  } else if (error.name === 'SequelizeUniqueConstraintError') {
+    return res.status(400).json({ error: 'Duplicate entry' })
   }
-})
+
+  next(error)
+}
+
+router.use(errorHandler)
 
 module.exports = router
