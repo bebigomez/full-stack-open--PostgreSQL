7fcaa6972b5356a5a45c232409d127a3eb032336
Victor Gomez <vhgomez.py@gmail.com>
2024-07-06 10:44:42 -0400

    13.19 done

--- notes-app/controllers/users.js
+++ notes-app/controllers/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const { tokenExtractor } = require('../util/middleware')
 
-const { User, Note } = require('../models')
+const { User, Note, Team } = require('../models')
 
 const isAdmin = async (req, res, next) => {
   const user = await User.findByPk(req.decodedToken.id)
@@ -14,8 +14,8 @@ const isAdmin = async (req, res, next) => {
 router.put('/:username', tokenExtractor, isAdmin, async (req, res) => {
   const user = await User.findOne({
     where: {
-      username: req.params.username
-    }
+      username: req.params.username,
+    },
   })
 
   if (user) {
@@ -29,10 +29,19 @@ router.put('/:username', tokenExtractor, isAdmin, async (req, res) => {
 
 router.get('/', async (req, res) => {
   const users = await User.findAll({
-    include: {
-      model: Note,
-      attributes: { exclude: ['userId'] },
-    },
+    include: [
+      {
+        model: Note,
+        attributes: { exclude: ['userId'] },
+      },
+      {
+        model: Team,
+        attributes: ['name', 'id'],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
   })
   res.json(users)
 })
@@ -47,7 +56,35 @@ router.post('/', async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
-  const user = await User.findByPk(req.params.id)
+  const user = await User.findByPk(req.params.id, {
+    attributes: { exclude: [''] },
+    include: [
+      {
+        model: Note,
+        attributes: { exclude: ['userId'] },
+      },
+      {
+        model: Note,
+        as: 'marked_notes',
+        attributes: { exclude: ['userId']},
+        through: {
+          attributes: [],
+        },
+        include: {
+          model: User,
+          attributes: ['name'],
+        },
+      },
+      {
+        model: Team,
+        attributes: ['name'],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  })
+
   if (user) {
     res.json(user)
   } else {
